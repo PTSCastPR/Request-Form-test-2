5797507f93e5148d7aa417297f466a56d8d54d7a
PTSCastPR <ramon.parga@ptsolutions.mx>
2024-06-17 18:07:16 -0600

    Add files via upload

---
+++ Request forms test3.js
@@ -0,0 +1,11 @@
+document.getElementById('mvr-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('MVR Request Submitted!');
+    // Add code to send email notifications and handle form data submission
+});
+
+document.getElementById('feedback-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Feedback Submitted!');
+    // Add code to handle feedback submission
+});
